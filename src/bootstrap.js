8b7b4d8e4496b88786be4d369dcc7916472a6ffd
Furkan_Onur <onurtozlu@hotmail.com>
2024-07-23 02:37:47 +0300

    update

---
+++ src/bootstrap.js
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+
+function Bootstrap () {
+   return (
+    <div>
+    <h3>FUNCTİON BOOTSTRAP</h3>
+    <Button variant="primary">Primary</Button>{' '}
+</div>
+   )
+
+};
+
+export default Bootstrap
---
+++ src/styles.js
@@ -0,0 +1,28 @@
+import { Title } from './component';
+import Bootstrap from './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
+function styles () {
+    return (
+        <div className="App">
+      <header className="App-header">
+        <Bootstrap />
+        <Title>Selamclar</Title>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Editdgit  e <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React ee
+        </a>
+      </header>
+      <Bootstrap />
+    </div>
+    )
+}
